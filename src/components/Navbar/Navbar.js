a48c86624fedfd009b5a39e78cf721a29f8d1bdd
Louis Perrenot <louis.perrenot@edu.devinci.net>
2023-02-03 16:28:00 +0100

    feat(Content): Add Navbar and Body

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <>
+            <nav>
+                <ul style={{display: 'flex', justifyContent: 'space-around'}}>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Store</a></li>
+                    <li><a href="/">Contact</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
